f747f572cf90448dde3b6f9c425567c74783c4af
daanieldesign <mahdakdaniel@gmail.com>
2023-11-09 13:31:04 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyButton from "./components/button";
+import NavigationMenu from "./components/navigation_menu";
+import HomePage from "./pages/homepage";
+
+function App() {
+  return (
+    <div className="App">
+    <MyButton/>
+    </div>
+  );
+}
+
+export default App;

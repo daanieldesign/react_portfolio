0630460f28afa785c1d1457e9402c56e6bea57ee
daanieldesign <mahdakdaniel@gmail.com>
2024-01-04 13:22:23 +0100

    update

--- src/components/pages/homepage.jsx
+++ src/components/pages/homepage.jsx
@@ -4,29 +4,31 @@ import Skill from "../skills/skillset";
 import './homepage_style.css';
 import Carousel from "../carousel/carousel";
 import ButtonBootStrap from "../Button/buttons";
+
 function HomePage() {
     return (
-        <div>
-            <section id="section1">
+        <div className="homepage-container">
+            <section id="section1" className="section">
                 <h2>Dovednosti</h2>
                 {
                     <Skill/>
                 }
             </section>
-            <section id="section2">
+            <section id="section2" className="section">
                 <h2>Sekce 2</h2>
                 {<p>Sekce2</p>}
             </section>
-            <section id="section3">
+            <section id="section3" className="section">
                 <h2>Sekce 3</h2>
                 {<p>Sekce3</p>}
             </section>
             <NavigationMenu/>
             {<Carousel/>}
-            <div>
-            <ButtonBootStrap/>
-        </div>
+            <div className="button-container">
+                <ButtonBootStrap/>
+            </div>
         </div>
     );
 }
+
 export default HomePage;

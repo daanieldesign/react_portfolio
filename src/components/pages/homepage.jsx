ccc85dbdee6a63d235ed56c8e0cbf7c0eb4614c5
daanieldesign <mahdakdaniel@gmail.com>
2024-01-11 13:27:16 +0100

    update

--- src/components/pages/homepage.jsx
+++ src/components/pages/homepage.jsx
@@ -4,23 +4,22 @@ import Skill from "../skills/skillset";
 import './homepage_style.css';
 import Carousel from "../carousel/carousel";
 import ButtonBootStrap from "../Button/buttons";
+import Header from "../Header/header";
 
 function HomePage() {
     return (
         <div className="homepage-container">
+            <Header/>
             <section id="section1" className="section">
-                <h2>Dovednosti</h2>
-                {
                     <Skill/>
-                }
             </section>
             <section id="section2" className="section">
-                <h2>Sekce 2</h2>
-                {<p>Sekce2</p>}
+                <h2>nevim</h2>
+                {<p>nevim</p>}
             </section>
             <section id="section3" className="section">
-                <h2>Sekce 3</h2>
-                {<p>Sekce3</p>}
+                <h2>nevim</h2>
+                {<p>nevim</p>}
             </section>
             <NavigationMenu/>
             {<Carousel/>}
